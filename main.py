1299393951f5b6fd7ec9fdb26dae9d969c5f063e
maxmolander <122946943+maxmolander@users.noreply.github.com>
2023-01-17 21:33:09 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,46 +2,51 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import simpleaudio as aud
-import musicLogic as mus                            #my library of music theory logic funtions
+from typing import Dict, List, Optional
+import musicLogic as mus  #my library of music theory logic funtions
+
+bpm = 100.0   #beats per minute
+beats = 2.0   #number of beats per chord (default to 2)
+T = 60.0*beats/bpm   #Period 
+fs=44100     #sampling frequency             
+pi=np.pi     
+t=np.arange(0,T,1.0/fs)  #time samples
+wt = t * 2 * pi   #multiplier to get freq to w for sin functions
 
 
-bpm = 100.0                                         #beats per minute
-beats = 2.0                                         #number of beats per chord (default to 2)
-T = 60.0*beats/bpm                                  #Period based on number of beats at tempo 
-fs=44100                                            #sampling frequency             
-pi=np.pi                                            #pi
-t=np.arange(0,T,1.0/fs)                             #samples in time based of period and sampling frequenccy
-wt = t * 2 * pi                                     #multiplier to get freq to w for sin functions
+#Generate on-off and off-on eighth note rhythms with 0s and 1s
+eighths = mus.twoEighthNotesOnBeat(T,fs)   
+eighthsRev = mus.twoEightNotesOffBeat(T,fs)   
 
-eighths = mus.twoEighthNotesOnBeat(T,fs)            #1s and 0s to multiply for on-off eighth notes (assuming beats = 2)
-eighthsRev = mus.twoEightNotesOffBeat(T,fs)         #0s and 1s to multiply for off-on eighth notes (assuming beats = 2)
+#hold chord, multiply by rhythm, add with arpeggiator on opposite rhythm
+chord1 = mus.ChordHold("F#", "minor", fs, T, wt)  
+chord1 = chord1 * eighths   
+chord1arp = mus.ArpEightSteps("F#", "minor", fs, T) 
+chord1arp = chord1arp * eighthsRev  
+chord1 = chord1/2 + chord1arp/2 
 
-chord1 = mus.ChordHold("F#", "minor", fs, T, wt)    #hold chord with those notes
-chord1 = chord1 * eighths                           #multiply to make ryhthm eighth notes
-chord1arp = mus.ArpEightSteps("F#", "minor", fs, T) #generate arppegio with same notes
-chord1arp = chord1arp * eighthsRev                  #multiply to make rhythm offbeat eighth notes
-chord1 = chord1/2 + chord1arp/2                     #add chord beats to arp beats for whole 2 beat section
-
-chord2arp = mus.ArpEightSteps("B","minor7",fs,T)    #arpeggiate chord with new notes
-#chord2arp = chord2arp * eighths
+chord2arp = mus.ArpEightSteps("B","minor7",fs,T) 
 chord2 = mus.ChordHold("B", "minor", fs, T, wt) 
 chord2 = chord2 * eighthsRev
 chord2 = chord2/2 + chord2arp
 
 chord3 = mus.ChordHold("E", "7", fs, T, wt)
 chord3 = chord3 * eighths
-chord3 = chord3/2 + mus.ArpEightSteps("E", "7", fs, T)/2
+chord3 = chord3/2 + mus.ArpEightSteps("G", "7", fs, T)/2
 
-chord4 = mus.ArpEightSteps("A", "Major7", fs, T)
-chord4 = chord4/2 + mus.ChordHold("A", "Major7", fs, T, wt)/2
+chord4arp = mus.ArpEightSteps("A", "Major7", fs, T)
+chord4 = mus.ChordHold("A", "Major7", fs, T, wt)
+chord4 = chord4arp/2 + chord4/2
 
-audio=np.hstack((chord1, chord2, chord3, chord4))   #concatenate chords together
-audio = audio * (2**15 - 1) / np.max(np.abs(audio)) #normalize (copy-pasted)
-audio = audio.astype(np.int16)                      #16-bit audio
-play_obj = aud.play_buffer(audio,1,2,fs)            #play audio until done
+#concatenate chords together, normalize, 16-bit audio, play until done
+audio=np.hstack((chord1, chord2, chord3, chord4))   
+audio = audio * (2**15 - 1) / np.max(np.abs(audio)) 
+audio = audio.astype(np.int16)                      
+play_obj = aud.play_buffer(audio,1,2,fs)           
 play_obj.wait_done()
 
-#fig = px.line(audio)                               #plot waveform
+#plot waveform
+#fig = px.line(audio)                               
 #fig.show()
 
 
--- musicLogic.py
+++ musicLogic.py
@@ -2,101 +2,79 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import simpleaudio as aud
+from typing import Dict, List, Optional
 
-#finds f1 fundamental frequency for any musical note letter name
-def noteToFreq(noteName):
-    if (noteName == "A"):
-        f1 = 220.0
-    elif (noteName == "A#" or noteName == "Bb"):
-        f1 = 233.08
-    elif (noteName == "B"):
-        f1 = 246.94
-    elif (noteName == "C"):
-        f1 = 261.63
-    elif (noteName == "C#" or noteName == "Db"):
-        f1 = 277.18
-    elif (noteName == "D"):
-        f1 = 293.66
-    elif (noteName == "D#" or noteName == "Eb"):
-        f1 = 311.13
-    elif (noteName == "E"):
-        f1 = 329.63
-    elif (noteName == "F"):
-        f1 = 349.23
-    elif (noteName == "F#" or noteName == "Gb"):
-        f1 = 369.99
-    elif (noteName == "G"):
-        f1 = 196.0
-    elif (noteName == "G#" or noteName == "Ab"):
-        f1 = 207.65
-    else:
-        print("Please input a valid note name")
-    
-    return f1
+#maps f1 fundamental frequency for any musical note letter name
+noteToFreq: Dict[str, float] = {
+    "A": 220.0,
+    "A#": 233.08,
+    "Bb": 233.08,
+    "B": 246.94,
+    "C": 261.63,
+    "C#": 277.18,
+    "Db": 277.18,
+    "D": 293.66,
+    "D#": 311.13,
+    "Eb": 311.13,
+    "E": 329.63,
+    "F": 349.23,
+    "F#": 369.99,
+    "Gb": 369.99,
+    "G": 196.0,
+    "G#": 207.65,
+    "Ab": 207.65
+}
 
-#calculates f2-8 based on f1 and chord quality
-def chordBuilder8notes(noteName, Quality):
-    f1 = noteToFreq(noteName)
-    if (Quality == "Major7"):
-        f2=f1*5/4
-        f3=f1*3/2
-        f4=f1*15/8
-        f5=f1*5/2
-        f6=f1*3
-        f7=f1*15/4
-        f8=f1*5
-    elif (Quality == "Major"):
-        f2=f1*5/4
-        f3=f1*3/2
-        f4=f1*2
-        f5=f1*5/2
-        f6=f1*3
-        f7=f1*4
-        f8=f1*5
-    elif (Quality == "minor"):
-        f2=f1*6/5
-        f3=f1*3/2
-        f4=f1*2
-        f5=f1*12/5
-        f6=f1*3
-        f7=f1*4
-        f8=f1*24/5
-    elif (Quality == "minor7"):
-        f2=f1*6/5
-        f3=f1*3/2
-        f4=f1*7/4
-        f5=f1*12/5
-        f6=f1*3
-        f7=f1*7/2
-        f8=f1*24/5
-    elif (Quality == "7"):
-        f2=f1*5/4
-        f3=f1*3/2
-        f4=f1*7/4
-        f5=f1*5/2
-        f6=f1*3
-        f7=f1*7/2
-        f8=f1*5
-    else:
+def getFreqForNote(noteName) -> Optional[float]:
+    if noteName not in noteToFreq:
+        print ("Please input a valid note name")
+        return None
+    return noteToFreq[noteName]
+
+#maps frequency ratios based on chord quality
+qualToRatios: Dict[str, List[float]] = {
+    "Major":    [1.0, 5/4, 3/2, 2, 5/2, 3, 4, 5],
+    "Major7":  [1.0, 5/4, 3/2, 15/8, 5/2, 3, 15/4, 5],
+    "7":        [1.0, 5/4, 3/2, 7/4, 5/2, 3, 7/2, 5],
+    "minor":    [1.0, 6/5, 3/2, 2, 12/5, 3, 4, 24/5],
+    "minor7":   [1.0, 6/5, 3/2, 7/4, 12/5, 3, 7/2, 24/5] 
+}
+
+def getRatiosForQuality(quality) -> Optional[List[float]]:
+    if quality not in qualToRatios:
         print("Please input a valid chord quality(Major, Major7, minor, minor7, or 7)")
-    
-    return [f2, f3, f4, f5, f6, f7, f8]
+        return None
+    return qualToRatios[quality]
+
+def chordBuilder8notes(noteName, quality) -> Optional[List[float]]:
+    f1 = getFreqForNote(noteName)
+    if f1 is None:
+        return None
+
+    ratios = getRatiosForQuality(quality)
+    if ratios is None:
+        return None
+
+    return [f1 * m for m in ratios]
 
 #combine and hold chord for T based on 
 # calls: noteToFreq, chodBuilder8notes
-def ChordHold(noteName, Quality, fs, T, wt):
-    #find fundamental frequency based on note name e.g. C or Eb
-    f1 = noteToFreq(noteName)
-    
-    #build chord based on frequency ratios associated with chord quality e.g. Major7
-    eightNoteChord = chordBuilder8notes(noteName, Quality)  #returns f2-8
-    
-    #start with sine wave of fundatmental
-    notes = np.sin(f1 * wt)
+def ChordHold(noteName, quality, fs, T, wt):
+    f1 = getFreqForNote(noteName)
 
-    #add sine waves of the other 7 notes, weighted
-    for i in eightNoteChord:
-        notes = notes + (np.sin(i * wt)/(i*2/f1))  #weights lower notes louder based on ratio to fundamental
+    if f1 is None:
+        return None
+    
+    #build chord based on frequency ratios 
+    eightNoteChord = chordBuilder8notes(noteName, quality)  
+    if eightNoteChord is None:
+        return None
+    
+    notes = 0 * wt
+
+    #add sine waves for each of 8 notes, weighted lower is louder
+    for f in eightNoteChord:
+        notes = notes + (np.sin(f * wt)/(f*2/f1))  
 
     return notes[0:int(T*fs)]
 
@@ -119,14 +97,19 @@ def twoEightNotesOffBeat(T,fs):
     envelope = np.hstack((envelope, envelope))
     return envelope[0:int(T*fs)]
 
-#arpeggiates through all 8 notes in the given numbeer of beats
-def ArpEightSteps(noteName, Quality, fs, T):
-    f1 = noteToFreq(noteName)
-    eightNoteChord = chordBuilder8notes(noteName, Quality)
-    t=np.arange(0,T/8.0,1.0/fs)                             #samples in time based of period and sampling frequenccy
-    wt = t * 2 * np.pi                                     #multiplier to get freq to w for sin functions  
-    notes = np.sin(f1*wt)
-    for i in eightNoteChord:
-        notes = np.hstack((notes, np.sin(i * wt)))
+#arpeggiates up through all 8 notes in the given numbeer of beats
+def ArpEightSteps(noteName, quality, fs, T):
+    f1 = getFreqForNote(noteName)
+    eightNoteChord = chordBuilder8notes(noteName, quality)
+    t=np.arange(0,T/8.0,1.0/fs)   
+    wt = t * 2 * np.pi    
+    notes = [0]
+
+    if eightNoteChord is None:
+            return None
+    
+    for f in eightNoteChord:
+        notes = np.hstack((notes, np.sin(f * wt)))
+
     return notes[0:int(T*fs)]
 
